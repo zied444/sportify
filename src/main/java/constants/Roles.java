4a84158b2a563bb72c0900c7e75b39dbda6b4253
zied444 <zied.elwakkel@esprit.tn>
2025-04-29 02:06:02 +0100

    Delete src directory

--- src/main/java/constants/Roles.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public class Roles {
-    public static final String ADMIN = "Admin";
-    public static final String ATHLETE = "Athlete";
-    public static final String COACH = "Coach";
-
-}
